07518ad11c536f13979beb596ac76c489cba295b
Valeryaleshka <valeryaleshka@gmail.com>
2020-10-12 15:26:17 +0300

    init: 1st version

--- src/index.js
+++ src/index.js
@@ -37,9 +37,46 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
+
 function decode(expr) {
-    // write your solution here
-}
+
+  
+
+    let expressions = expr.split("**********");
+    let expressions2 = [expressions.length];
+    let temp = [];
+    
+    let z = ""
+    let zero = new RegExp("00", 'g');
+    let dot = new RegExp("10", 'g');
+    let dash = new RegExp("11", 'g');
+   
+    for(let i = 0 ; i< expressions.length;i++){
+                       
+        while (expressions[i].length>0){
+            z = expressions[i].substr(0,10).replace(zero,"").replace(dot,".").replace(dash,"-");            
+            expressions[i] = expressions[i].slice(10);
+            temp.push(z);
+        }
+        
+        expressions2[i] = temp;
+        temp = [];
+    } 
+    
+    for (let i = 0 ; i< expressions2.length;i++){
+        for (let j = 0 ; j< expressions2[i].length;j++){
+            expressions2[i][j] = MORSE_TABLE[expressions2[i][j]];
+        }
+    }
+    
+    for( let i = 0; i< expressions2.length; i++){
+        expressions2[i] = expressions2[i].join("");
+    }
+
+    return expressions2.join(" ");
+
+    }
 
 module.exports = {
     decode
